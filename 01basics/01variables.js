003192aea984062b2e8dbd6449e77da76458dac3
Sahin <87063268+yourgeekdad@users.noreply.github.com>
2023-10-04 16:15:24 +0000

    01variables

---
+++ 01basics/01variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail= "sahin@gmmail.com"
+var accountPassword= "12345"
+acountCity="tezpur"
+let accountState
+
+// accountId= 56453 // not allowed 
+
+accountEmail="sakiza@gmail.com"
+accountPassword="2580"
+accountCity="darrang"
+
+
+
+
+console.log("accountId");
+
+console.table([accountId,accountEmail, accountPassword, accountCity, accountState]);
