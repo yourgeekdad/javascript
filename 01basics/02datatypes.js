b4c6e954ec46d0a3179249eef1a4dea6c28e646f
Sahin <87063268+yourgeekdad@users.noreply.github.com>
2023-10-04 17:25:24 +0000

    datatypes

---
+++ 01basics/02datatypes.js
@@ -0,0 +1,29 @@
+"use strct" // treat all JS as newer version.
+
+// alert (3+3)// we are using nodejs, not browser
+
+console.log (3
+    +
+    3)// code readability should be high
+
+
+console.log("sahin")
+
+let name= "sahin" 
+let age = 21 
+let isloggedIn= false
+let state;
+
+// number => 2 to power 53
+//bigint
+// string => " "
+// boolean => true/false
+// null => standalone value(empty)
+// undefined => 
+// symbol => unique
+//  all are primitive datatypes
+
+
+// object
+
+console.log(typeof undefined );
